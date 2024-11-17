34948bfce8fcce801ca30a40413fecf931b28f22
TruongTuan003 <21030271.tuan@student.iuh.edu.vn>
2024-11-17 15:40:56 +0700

    create Project form

---
+++ src/main/java/com/iuh/ProjectWwwBanNhacCuApplication.java
@@ -0,0 +1,13 @@
+package com.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWwwBanNhacCuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWwwBanNhacCuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iuh/ProjectWwwBanNhacCuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWwwBanNhacCuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
